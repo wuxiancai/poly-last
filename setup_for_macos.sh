4d9a4ef9b99f73c708436311108205807c7297ef
xiancai <36890537+wuxiancai@users.noreply.github.com>
2025-05-16 18:04:27 +0800

    Update setup_for_macos.sh

--- setup_for_macos.sh
+++ setup_for_macos.sh
@@ -51,7 +51,7 @@ brew update
 echo "安装 Python 3.9..."
 brew install python@3.9 --force
 brew link --force --overwrite python@3.9
-# 安装 python-tk@3.9 (自动模式)
+echo "安装 python-tk@3.9 (自动模式)"
 brew install python-tk@3.9 --force
 brew install wget
 
