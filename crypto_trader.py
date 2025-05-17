ea102fb16428be87b1b698a1754a117a84adc972
xiancai <36890537+wuxiancai@users.noreply.github.com>
2025-05-17 11:08:04 +0800

    Update crypto_trader.py

--- crypto_trader.py
+++ crypto_trader.py
@@ -3246,7 +3246,7 @@ class CryptoTrader:
         """安排每天1点2分执行自动找币"""
         now = datetime.now()
         # 计算下一个3点2分的时间
-        next_run = now.replace(hour=9, minute=23, second=0, microsecond=0)
+        next_run = now.replace(hour=0, minute=23, second=0, microsecond=0)
         if now >= next_run:
             next_run += timedelta(days=1)
         
